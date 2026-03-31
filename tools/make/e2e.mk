5f28527ac14b7bbd1514db4e979a940699063875
yuluo-yx <yuluo08290126@gmail.com>
2026-03-31 21:48:09 +0800

    add .github dir
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- tools/make/e2e.mk
+++ tools/make/e2e.mk
@@ -1,16 +1,16 @@
 ##@ e2e
 
-PHONY: build-e2e-image
+.PHONY: build-e2e-image
 build-e2e-image: ## Build the Docker image for end-to-end tests
 	@$(LOG_TARGET)
-  docker build -f e2e/Dockerfile -t $(E2E_DOCKER_IMAGE) .
+	docker build -f e2e/Dockerfile -t $(E2E_DOCKER_IMAGE) .
 
-PHONY: test-e2e
+.PHONY: test-e2e
 test-e2e: ## Run end-to-end tests locally
 	@$(LOG_TARGET)
 	$(GO) test ./e2e -v
 
-PHONY: test-e2e-docker
+.PHONY: test-e2e-docker
 test-e2e-docker: build-e2e-image
 test-e2e-docker: ## Run end-to-end tests in a Docker container
 	@$(LOG_TARGET)
