c76956412ccee06a8d6d557c5c629926e0f11ba5
yuluo-yx <yuluo08290126@gmail.com>
2026-04-05 22:49:44 +0800

    fix
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- tools/make/tools.mk
+++ tools/make/tools.mk
@@ -5,5 +5,5 @@
 .PHONY: install-golanglint
 install-golanglint: ## Install golangci-lint
 	@$(LOG_TARGET)
-	@command -v golangci-lint >/dev/null 2>&1 || go install github.com/golangci/golangci-lint/cmd/golangci-lint@main
+	@command -v golangci-lint >/dev/null 2>&1 || go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest
 	@golangci-lint --version
