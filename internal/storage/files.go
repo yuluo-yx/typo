cfd7c34acdaf1ecae72ae042dffa048e1bbd3379
shown <yuluo08290126@gmail.com>
2026-06-06 16:19:07 +0800

    chore(dev): sync Go version docs and storage coverage (#176)

--- internal/storage/files.go
+++ internal/storage/files.go
@@ -7,11 +7,43 @@ import (
 	"time"
 )
 
+type atomicFile interface {
+	Name() string
+	Chmod(os.FileMode) error
+	Write([]byte) (int, error)
+	Sync() error
+	Close() error
+}
+
+type atomicFileOps interface {
+	createTemp(dir, pattern string) (atomicFile, error)
+	rename(oldpath, newpath string) error
+	remove(name string) error
+}
+
+type osAtomicFileOps struct{}
+
+func (osAtomicFileOps) createTemp(dir, pattern string) (atomicFile, error) {
+	return os.CreateTemp(dir, pattern)
+}
+
+func (osAtomicFileOps) rename(oldpath, newpath string) error {
+	return os.Rename(oldpath, newpath)
+}
+
+func (osAtomicFileOps) remove(name string) error {
+	return os.Remove(name)
+}
+
 // WriteFileAtomic writes the target file atomically by renaming a temp file in the same directory.
 func WriteFileAtomic(filename string, data []byte, perm os.FileMode) error {
+	return writeFileAtomicWithOps(filename, data, perm, osAtomicFileOps{})
+}
+
+func writeFileAtomicWithOps(filename string, data []byte, perm os.FileMode, ops atomicFileOps) error {
 	dir := filepath.Dir(filename)
 
-	tmpFile, err := os.CreateTemp(dir, filepath.Base(filename)+".tmp-*")
+	tmpFile, err := ops.createTemp(dir, filepath.Base(filename)+".tmp-*")
 	if err != nil {
 		return err
 	}
@@ -20,7 +52,7 @@ func WriteFileAtomic(filename string, data []byte, perm os.FileMode) error {
 	keepTemp := false
 	defer func() {
 		if !keepTemp {
-			_ = os.Remove(tmpName)
+			_ = ops.remove(tmpName)
 		}
 	}()
 
@@ -39,7 +71,7 @@ func WriteFileAtomic(filename string, data []byte, perm os.FileMode) error {
 	if err := tmpFile.Close(); err != nil {
 		return err
 	}
-	if err := os.Rename(tmpName, filename); err != nil {
+	if err := ops.rename(tmpName, filename); err != nil {
 		return err
 	}
 
